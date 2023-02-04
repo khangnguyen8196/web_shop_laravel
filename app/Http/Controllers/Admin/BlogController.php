5d1d2fb5184734650d09a939af83ad86d12bddc3
Nguyenhoangphuong <khangnguyen8196@gmail.com>
2023-02-04 17:14:01 +0700

    test2

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -36,4 +36,27 @@ class BlogController extends Controller
         }
         return view('admin.news.blog');
     }
+
+    public function store(Request  $request)
+    {
+        $data = Blog::get();
+        if($request->ajax())
+        {
+            $allData = Datatables::of( $data)
+            ->addIndexColumn()
+            ->addColumn('action', function($row){
+
+                   $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editBlog">Edit</a>';
+
+                   $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteBlog">Delete</a>';
+
+                    return $btn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+            return $allData;
+
+        }
+        return view('admin.news.blog');
+    }
 }
